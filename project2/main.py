16367d00ed029746b721673e598dc3201ce2a8d2
paoloprogram12 <sethpsalazar@gmail.com>
2025-06-19 23:16:42 -0700

    setup

--- project2/main.py
+++ project2/main.py
@@ -1,6 +1,6 @@
-def main():
-    print("Hello from project2!")
-
-
-if __name__ == "__main__":
-    main()
+import streamlit as st
+import PyPDF2
+import io
+import os
+from openai import OpenAI
+from dotenv import load_dotenv
