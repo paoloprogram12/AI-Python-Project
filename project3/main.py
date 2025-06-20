3eea45a30bfbfdd3d6ab3e588172ecb547213bfc
paoloprogram12 <sethpsalazar@gmail.com>
2025-06-20 16:58:27 -0700

    main setup

--- project3/main.py
+++ project3/main.py
@@ -1,6 +1,13 @@
-def main():
-    print("Hello from project3!")
+# tensorflow is a pre-trained machine learning model that can look at an image and classify it
+# opencv allows manipulations on the image that we load, so that it can get passed through tensorflow
 
+import cv2
+import numpy as np
+import streamlit as st
+from tensorflow.keras.applications.mobilenet_v2 import (
+    MobileNetV2,
+    preprocess_input,
+    decode_predictions
+)
+from PIL import Image
 
-if __name__ == "__main__":
-    main()
